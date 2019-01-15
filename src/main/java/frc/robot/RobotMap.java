0693e8bb15c8bb681275482127587792d087b05a
Trogdor14 <emu1402@gmail.com>
2019-01-14 19:09:58 -0500

    Added Xbox controllers

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -13,6 +13,7 @@ package frc.robot;
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  */
+
 public class RobotMap {
   // For example to map the left and right motors, you could define the
   // following variables to use with your drivetrain subsystem.
@@ -23,4 +24,29 @@ public class RobotMap {
   // number and the module. For example you with a rangefinder:
   // public static int rangefinderPort = 1;
   // public static int rangefinderModule = 1;
+
+  // JoystickAxis
+  public static final int X_LEFT_AXIS = 0; 
+  public static final int Y_LEFT_AXIS = 1;
+  public static final int X_RIGHT_AXIS = 4;
+  public static final int X_LEFT_AXIS = 5;
+
+  /* 
+  //this is stuff for logitech that we might not use this year
+  public static final int X_AXIS_LOGITECH = 0;
+	public static final int Y_AXIS_LOGITECH = 1;
+	public static final int Z_AXIS_LOGITECH = 2;
+  public static final int SLIDER_AXIS_LOGITECH = 3;
+  
+  //Logitech Axis
+	public static final int X_AXIS = 0;
+  public static final int Y_AXIS = 1;
+  
+  //basic pneumatic stuff
+  public static final int PNEUMATICCLAW_OPEN = 2;
+  public static final int PNEUMATICCLAW_CLOSED = 3; 
+  public static final int PNEUMATICCLAW_RELEASE_OPEN = 4;
+  */ 
+
+
 }
